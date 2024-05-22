68343f59dcdc54883aec41d57a2b615807185960
Crowdbotics <team@crowdbotics.com>
2024-05-22 13:50:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "suuport-beta-48059",
+  url: "https://suuport-beta-48059.botics.co",
+  api: "https://suuport-beta-48059.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
